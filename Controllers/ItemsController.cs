16904f3e0663963571d29af8057dde9bd7fddace
Äkwav <ekwav@coflnet.com>
2022-06-01 17:51:50 +0200

    bump cache time

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -82,7 +82,7 @@ namespace Coflnet.Sky.Items.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        [ResponseCache(Duration = 18, Location = ResponseCacheLocation.Any, NoStore = false)]
+        [ResponseCache(Duration = 1800, Location = ResponseCacheLocation.Any, NoStore = false)]
         [Route("/item/{itemTag}/modifiers/all")]
         public async Task<Dictionary<string, HashSet<string>>> Modifiers(string itemTag)
         {
