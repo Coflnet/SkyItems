3594f078e95820f6173457baf6862ef0c022fd2c
Äkwav <ekwav@coflnet.com>
2022-06-17 19:30:49 +0200

    remove rune levels

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -150,6 +150,8 @@ namespace Coflnet.Sky.Items.Controllers
             if (fullName == null)
                 return null;
             var noSpecialChars = fullName.Trim('✪').Replace("⚚", "").Replace("✦", "");
+            if(fullName.Contains("Rune"))
+                noSpecialChars = noSpecialChars.TrimEnd('I').TrimEnd();
             return System.Text.RegularExpressions.Regex.Replace(noSpecialChars, @"\[Lvl \d{1,3}\] ", "").Trim(); ;
         }
 
